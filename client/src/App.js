67058db9f8cc8ae0a99b4d369cb324d06e298013
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-06-26 20:58:06 -0500

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,24 @@
+import Header from "../src/components/Header"
+import About from "../src/components/About"
+import Albums from "./components/Albums";
+import Singles from "./components/Singles";
+import Remixes from "./components/Remixes";
+
 function App() {
   return (
     <div className="App">
-      <div className="navbar bg-base-100">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      <Header/>
+ 
+      <div className="flex justify-center flex-wrap bg-customGreen p-4 sm:p-8">
+        <img className=" w-auto max-h-[20vh] inline-block animate-slideLeft" src="/images/Xandr.png" alt="Xandr" />
+        <img className=" w-auto max-h-[20vh] inline-block animate-slideRight ml-2" src="/images/Wolf.png" alt="Wolf" />
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
+
+      <About/>
+      <Albums/>
+      <Singles/>
+      <Remixes />
+
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,30 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGreen: '#174A43',
+      },
+      keyframes: {
+        slideRight: {
+          '0%': { marginLeft: '-400%' },
+          '100%': { marginLeft: '0%' },
+        },
+        slideLeft: {
+          '0%': { marginLeft: '300%' },
+          '100%': { marginLeft: '0%' },
+        },
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        slideRight: 'slideRight 1s ease-out',
+        slideLeft: 'slideLeft 1s ease-out',
+        fadeIn: 'fadeIn 1s ease-out',
+      },   
+    },
   },
   daisyui: {
     themes: [],
