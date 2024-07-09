1a92bf1ae5251c5a4b8be078bcfbe5b08190cf44
Miguel-C22 <110456521+Miguel-C22@users.noreply.github.com>
2024-07-08 20:06:50 -0500

    remove Banner

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -4,10 +4,6 @@ import { Link } from 'react-router-dom'
 function Login() {
   return (
   <div className='mb-12'>
-    <div className='absolute inset-0 flex flex-col justify-center items-center'>
-      <img className=" w-auto max-h-[15vh] sm:max-h-[20vh] inline-block animate-slideLeft" src="/images/Xandr.png" alt="Xandr" />
-      <img className=" w-auto max-h-[15vh] sm:max-h-[20vh] inline-block animate-slideRight ml-2" src="/images/Wolf.png" alt="Wolf" />
-    </div>
     <div className="flex flex-col justify-center items-center sm:max-w-2xl max-w-sm m-auto gap-12 bg-customGreen py-32 sm:p-32 rounded-lg mt-32 text-white relative bg-opacity-95">
       <h1 className='text-2xl font-bold'>Admin Login</h1>
       <form action="" className="flex flex-col w-full px-4 gap-2">
